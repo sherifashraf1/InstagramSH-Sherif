558ce8daf9df564da490597be2054f2fb128d48f
Sherif Ashraf <sherifashraf910@gmail.com>
2019-07-09 05:27:58 +0200

    Custom tableview cell using UINib

--- InstagramSH/NewsFeedCell.swift
+++ InstagramSH/NewsFeedCell.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class NewsFeedCell: UITableViewCell , UITextViewDelegate {
 
+//    @IBOutlet weak var userProfilePicture: UIImageView!
+//    @IBOutlet weak var userNamePostButton: UIButton!
+//    @IBOutlet weak var threeDotsButton: UIButton!
+//    @IBOutlet weak var userPostImage: UIImageView!
+//    @IBOutlet weak var likeButton: UIButton!
+//    @IBOutlet weak var commentButton: UIButton!
+//    @IBOutlet weak var shareButton: UIButton!
+//    @IBOutlet weak var saveButton: UIButton!
+//    @IBOutlet weak var numberOfLikesButton: UIButton!
+//    @IBOutlet weak var accountProfilePicture: UIImageView!
+//    @IBOutlet weak var addCommentTextView: UITextView!
+//    @IBOutlet weak var smileEmojiCommentButton: UIButton!
+//    @IBOutlet weak var heartEmojiCommentButton: UIButton!
+//    @IBOutlet weak var plusCommentButton: UIButton!
+//    @IBOutlet weak var timeAgoLabel: UILabel!
+//
+    
     @IBOutlet weak var userProfilePicture: UIImageView!
     @IBOutlet weak var userNamePostButton: UIButton!
     @IBOutlet weak var threeDotsButton: UIButton!
@@ -19,34 +36,39 @@ class NewsFeedCell: UITableViewCell , UITextViewDelegate {
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var numberOfLikesButton: UIButton!
+    @IBOutlet weak var postCaptionLabel: UILabel!
     @IBOutlet weak var accountProfilePicture: UIImageView!
     @IBOutlet weak var addCommentTextView: UITextView!
     @IBOutlet weak var smileEmojiCommentButton: UIButton!
     @IBOutlet weak var heartEmojiCommentButton: UIButton!
-    @IBOutlet weak var plusCommentButton: UIButton!
+    @IBOutlet weak var plusEmojiCommentButton: UIButton!
     @IBOutlet weak var timeAgoLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         userProfilePicture.layer.cornerRadius = userProfilePicture.frame.width / 2
+        addCommentConfigTextView()
+    }
+
+    func addCommentConfigTextView(){
         addCommentTextView.showsVerticalScrollIndicator = false
         addCommentTextView.text = "Add a comment..."
         addCommentTextView.textColor = .lightGray
         addCommentTextView.font = UIFont(name: "verdana", size: 13.0)
         addCommentTextView.returnKeyType = .done
         addCommentTextView.delegate = self
-    
+
     }
-    
+
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.text == "Add a comment..." {
             textView.text = ""
             textView.textColor = .black
             textView.font = UIFont(name: "verdana", size: 13.0)
-            
+
         }
-    
+
     }
-    
+
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text == "" {
             textView.text = "Add a comment..."
@@ -55,13 +77,13 @@ class NewsFeedCell: UITableViewCell , UITextViewDelegate {
 
         }
     }
-    
+
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if text == "\n" {
             textView.resignFirstResponder()
         }
         return true
-        
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -71,3 +93,4 @@ class NewsFeedCell: UITableViewCell , UITextViewDelegate {
     }
     
 }
+
--- InstagramSH/TabBarControllers/HomeViewController.swift
+++ InstagramSH/TabBarControllers/HomeViewController.swift
@@ -9,23 +9,28 @@
 import UIKit
 
 class HomeViewController: UIViewController , UITableViewDataSource , UITableViewDelegate {
-
+    
     var postImages : [String] = ["doaa" , "essam" , "me" , "sherif" , "amr_diab" , "hands" , "neymar" ,"cristiano"]
     var postUserName = ["Sherif Ashraf","Osama Ahmed","Ahmed Mohamed","Mahmoud Fekry","Ehab Nagi","Sherif Ahmed" , "Mohamed Gamal","Omar Ahmed"]
     
-
+    
     enum myTableSection : CaseIterable{ //CaseIterable is a protocol to use the enumu like as array the each case is an indexpath
         case stories                    // indexpath 0
         case newsFeed                   // indexpath 1
+        
+    }
+    
+    func changeInstagramTitleAndSizeOnNavBar(){
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "Noteworthy-Bold", size: 25) ?? "verdana"]
     }
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "Noteworthy-Bold", size: 20) ?? "verdana"]
-        
+        changeInstagramTitleAndSizeOnNavBar()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(UINib(nibName: "NewsFeedCell", bundle: nil), forCellReuseIdentifier: "NewsFeedCell")
         
     }
     
@@ -34,23 +39,21 @@ class HomeViewController: UIViewController , UITableViewDataSource , UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let section = myTableSection.allCases[section]
+        let section = myTableSection.allCases[section] // عايز اعرف هو واقف دلوقتى عند الاسكشن رقم كام
         switch section {
         case .stories :
             return 1
-
+            
         case .newsFeed :
             return postImages.count
         }
     }
     
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let section = myTableSection.allCases[indexPath.section]
+        let section = myTableSection.allCases[indexPath.section] // عايز اعرف هو واقف دلوقتى عند السكشن رقم كام
         
         switch section {
-            
         case .stories :
             let cell = tableView.dequeueReusableCell(withIdentifier: "StoryTableViewCell") as! StoryTableViewCell
             return cell
@@ -64,5 +67,5 @@ class HomeViewController: UIViewController , UITableViewDataSource , UITableView
         }
         
     }
-
+    
 }
